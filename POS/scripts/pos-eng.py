652f57d68aecccf68cf79f7f70807858df92b743
Alexander <alxndr.martin@gmail.com>
2017-11-13 12:57:28 +0000

    added comments to make code more readable

--- POS/scripts/pos-eng.py
+++ POS/scripts/pos-eng.py
@@ -22,10 +22,10 @@ def draw1ModSet(ID, df):
     return {'train': train, 'test': test}
 
 
-def commas(x):
-    'Turn list into comma-separated string.'
-    form = [str(i) for i in x]
-    return ','.join(form)
+# def commas(x):
+#     'Turn list into comma-separated string.'
+#     form = [str(i) for i in x]
+#     return ','.join(form)
     
 
 def instructies(x):
@@ -39,12 +39,15 @@ def instructies(x):
 
 
 def playStim(x):
+    # play sound and do not allow other processes to continue until sound finished
     x.play()
     core.wait(x.getDuration())
     return
 
 
 def makeButton(buttonName, buttonText):
+    # button names are A B C and D and have locations associated with them
+    # make autodrawing button (will have to be turned off to not display)
     button = visual.Rect(
         win,
         width=buttonWidth,
@@ -69,26 +72,31 @@ def makeButton(buttonName, buttonText):
 
     
 def getClick(mouse, buttons, cons, eng):
-    
+
+    # going to loop until click detected
     clicked = False
+    
     while not clicked:
         # button is a tuple where 0 is shape obj and 1 is text obj
         for n, button in enumerate(buttons):
+            # if mouse detected in shape (not clicked)
             if button[0].contains(mouse):
+                # make button dark grey
                 button[0].setFillColor(hoverColor)
                 win.flip()
             else:
+                # otherwise make button normal color
                 button[0].setFillColor(buttonColor)
                 win.flip()
+            # if click detected in button
             if mouse.isPressedIn(button[0]):
+                # then break the loop
                 clicked = True
+                # and get the response
                 responseButton = n
-                # print response
 
     responseText = buttons[responseButton][1]
                 
-   
-        
     win.flip()
     
     return responseButton, responseText.text
@@ -96,10 +104,10 @@ def getClick(mouse, buttons, cons, eng):
     
 def initializeTrial(displayText, buttonNames, buttonTexts):
 
+    # create mouse object (not active yet)
     mouse = event.Mouse(visible=False)
     
     win.flip()
-
    
     # draw text stimulus and consigne
     cons = visual.TextStim(
@@ -136,7 +144,7 @@ def initializeTrial(displayText, buttonNames, buttonTexts):
     
     
 def makePhrase(words):
-
+    # make list of sound objects based on words
     stimulus = [sound.Sound('../stimuli/'+i+'-trim.wav') for i in words]
 
     return stimulus
@@ -145,26 +153,31 @@ def makePhrase(words):
 
 def doTrainingTrial(noun, modifier, nTrial):
 
+    # wait 500 ms at beginning of trial
     core.wait(0.5)
-    
+
+    # prepare the English and the English' possibilities
     engText = modifier + ' ' + noun
     prenom = modifier + ' ' + noun
     postnom = noun + ' ' + modifier
 
     buttonTexts = [prenom, postnom]
+    # mix up button texts
     random.shuffle(buttonTexts)
-   
+
+    # create mouse and button objects, display instructions
     mouse, buttons, cons, eng = initializeTrial(
         displayText=engText,
         buttonNames=['A', 'B'],
         buttonTexts=['-----'] * len(buttonTexts)
     )
 
-    # sound
+    # make and play sound objects based on the English'
     stims = makePhrase([noun, modifier])
     for stim in stims:
         playStim(stim)
 
+    # wait 500 ms after sound
     core.wait(0.5)
 
     consBisText = "...click on the choice that matches what you heard... ({}/30)".format(nTrial+1)
@@ -185,29 +198,31 @@ def doTrainingTrial(noun, modifier, nTrial):
         
     # activate mouse
     mouse.setVisible(True)
-        
+
+    # recover response and response button
     responseButton, response = getClick(mouse, buttons, cons, eng)
-    # print response
+
+    # check to see that participant selected the postnominal variant
     if response == postnom:
         correct = 1
     else:
         correct = 0
 
-    print correct
-
+    # if correct, then turn button green
     if correct == 1:
-        buttons[responseButton][0].setFillColor('green')
+        buttons[responseButton][0].setFillColor('green') # no need to draw since AutoDraw active
+    # else turn button red
     else:
         buttons[responseButton][0].setFillColor('red')
 
     win.flip()
+    # wait 1000 ms before moving on to next trial    
     core.wait(1)
     
     # erase objects
     for button in buttons:
         button[0].setAutoDraw(False)
         button[1].setAutoDraw(False)
-
     cons.setAutoDraw(False)
     eng.setAutoDraw(False) 
     consBis.setAutoDraw(False)
@@ -218,19 +233,27 @@ def doTrainingTrial(noun, modifier, nTrial):
 
 def doTraining(nouns, modifiers):
 
+    # trainingDf will be updated by the function, so must be global
     global trainingDf
-    
-    i = 0
-    while i < 2:
-        modifier = modifiers.ix[i]
-        modifierWord = modifier.word
-        number = modifier.nb
-        if number == 'plur':
-            nounWord = nouns.ix[i].plur
-        else:
-            nounWord = nouns.ix[i].sing
 
+    # because we need to repeat incorrect trials, must have own counter
+    i = 0
+
+    # until counter reaches 30 correct trials, loop
+    while i < 2: # 30
+        # recover mod (the row includes category and number info)
+        modifier = modifiers.ix[i]
+        # recover actual word
+        modifierWord = modifier.word
+        # find if mod is plur or sing
+        number = modifier.nb
+        # recover noun that agrees in nb with mod
+        nounWord = nouns.ix[i][number]
+
+        # do the trial and recover button content
         response, correct, buttonA, buttonB = doTrainingTrial(nounWord, modifierWord, i)
+
+        # if trial correct, move on, else repeat
         if correct == 1:
             i += 1
         else:
@@ -255,8 +278,10 @@ def doTraining(nouns, modifiers):
 
 def doTestTrial(nTrial, noun, modOuter, modInner=None):
 
+    # wait 500 ms at beginning of trial
     core.wait(0.5)
 
+    # if there is an inner mod, four buttons
     if type(modInner) is str:
         buttonNames = ['A', 'B', 'C', 'D']
 
@@ -269,7 +294,7 @@ def doTestTrial(nTrial, noun, modOuter, modInner=None):
         
         buttonTexts = [prenom1, prenom2, postnom1, postnom2]
 
-        
+    # if not, only two buttons
     else:
         buttonNames = ['A', 'B']
 
@@ -280,16 +305,17 @@ def doTestTrial(nTrial, noun, modOuter, modInner=None):
 
         buttonTexts = [prenom, postnom]
 
-
+    # mix up button locations
     random.shuffle(buttonTexts)
-   
+
+    # create mouse and button objects, display instructions
     mouse, buttons, cons, eng = initializeTrial(
         displayText=engText,
         buttonNames=buttonNames,
         buttonTexts=['-----'] * len(buttonTexts)
     )
 
-
+    # wait 1000 ms before displaying possible answers
     core.wait(1)
 
     consBisText = "...click on the choice that the speaker would most likely say... ({}/50)".format(nTrial+1)
@@ -308,6 +334,7 @@ def doTestTrial(nTrial, noun, modOuter, modInner=None):
     for n,button in enumerate(buttons):
         button[1].text = buttonTexts[n]
 
+    # recover what was displayed on different buttons
     buttonA = buttonTexts[0]
     buttonB = buttonTexts[1]
     try:
@@ -319,19 +346,21 @@ def doTestTrial(nTrial, noun, modOuter, modInner=None):
         
     # activate mouse
     mouse.setVisible(True)
-        
+
+    # recover response
     responseButton, response = getClick(mouse, buttons, cons, eng)
 
+    # make response button blue after click
     buttons[responseButton][0].setFillColor('blue')
     
     win.flip()
+    # wait 1000 ms after response before moving on to next trial
     core.wait(1)
     
     # erase objects
     for button in buttons:
-        button[0].setAutoDraw(False)
-        button[1].setAutoDraw(False)
-
+        button[0].setAutoDraw(False) # erase shape
+        button[1].setAutoDraw(False) # erase text
     cons.setAutoDraw(False)
     eng.setAutoDraw(False) 
     consBis.setAutoDraw(False)
@@ -341,13 +370,17 @@ def doTestTrial(nTrial, noun, modOuter, modInner=None):
 
 
 def doTest(trials):
+    "Run through test trials and save data in test df."
 
+    # testDf will be updated by the function, so must be global
     global testDf
-    
-    for i in range(6): # trials.index
-        row = trials.ix[i]
-        response, responseButton, buttonA, buttonB, buttonC, buttonD = doTestTrial(i, row.noun, row.outer, row.inner)
 
+    # run through pre-determined trials
+    for i in range(6): # trials.index
+        row = trials.ix[i] # extract trial from df
+        # do trial and recover response information and location of possible responses
+        response, responseButton, buttonA, buttonB, buttonC, buttonD = doTestTrial(i, row.noun, row.outer, row.inner)
+        
         dico = {
             'trial':i,
             'noun':row.noun,
@@ -360,24 +393,14 @@ def doTest(trials):
             'response':response,
             'responseButton':responseButton
         }
+        # add subject info to final dico that will be a row in testDf
         dico.update(subjInfo)
         
         trial = pd.DataFrame([dico])
         testDf = testDf.append(trial)
     return
-
-
-
-
     
 #########################
-
-
-
-
-
-
-
 
 
 #########################
@@ -394,10 +417,6 @@ else:
     core.quit()
 
 #########################
-
-
-
-
 
 
 #########################
@@ -472,7 +491,6 @@ testCols = [
     'responseButton'
 ]
 testDf = pd.DataFrame(columns=testCols)
-
 
 
 #########################
@@ -592,6 +610,71 @@ buttonColor = "lightgrey"
 #########################
 
 
+#########################
+## DEFINE INSTRUCTIONS ##
+
+welcome = u'''Welcome!
+
+This is an experiment about learning a small part of a new language. It will take about 30 minutes to complete and you will be paid £5.00 for your time. This experiment is part of a series of studies being conducted by Dr Jennifer Culbertson at the University of Edinburgh, and has been approved by the Linguistics and English Language Ethics Committee. 
+
+Proceeding indicates that:
+
+- you are a native speaker of English, at least 18 years old
+- you have read the information letter
+- you voluntarily agree to participate, and understand you can stop your participation at any time
+- you agree that your anonymous data may be kept permanently in Edinburgh University archives and may used by qualified researchers for teaching and research purposes
+
+If you do not agree to all of these, please inform the experimenter now.
+
+If you agree, press the spacebar.'''
+
+##
+
+intro = u'''In this experiment, you will be learning part of a new language.  The language is similar to English, but you will notice some differences.
+
+Your task will be to learn to translate from English into the new language.
+
+Press the spacebar to continue.
+'''
+
+##
+
+consigne = u'''Instructions -- please read carefully!
+
+Now you'll see a phrase in English, and hear a speaker of the language you're learning translate it.
+
+Look at the English phrase, listen to the speaker translate it, and then click on the translation that matches what you heard the speaker say.
+
+It's important to pay close attention, so that later on you'll be able to translate on your own.
+
+Press the spacebar to continue.
+'''
+
+##
+
+testConsigne = u'''Instructions -- please read carefully!
+
+In the next part of the experiment, you will show what you have learned about this new language.
+
+You will see an English phrase -- it may be the SAME LENGTH phrase that you have seen before, or it may be LONGER.
+
+Look at the English phrase, and click on the translation that you think a speaker of the language WOULD BE MOST LIKELY TO SAY.
+
+Try to do as well as you can, remembering what you learned in the first part of the experiment, but don't worry if once in a while you have to guess.
+
+Press the spacebar to continue.
+'''
+
+##
+
+end = u'''Thank you!
+
+Please see the experimenter, who will proceed with a questionnaire about your linguistic experience.
+
+Press the spacebar to exit the experiment.
+'''
+
+#########################
 
 
 #########################
@@ -616,72 +699,31 @@ else:
 win.flip()
 
 
-welcome = u'''Welcome!
-
-This is an experiment about learning a small part of a new language. It will take about 30 minutes to complete and you will be paid £5.00 for your time. This experiment is part of a series of studies being conducted by Dr Jennifer Culbertson at the University of Edinburgh, and has been approved by the Linguistics and English Language Ethics Committee. 
-
-Proceeding indicates that:
-
-- you are a native speaker of English, at least 18 years old
-- you have read the information letter
-- you voluntarily agree to participate, and understand you can stop your participation at any time
-- you agree that your anonymous data may be kept permanently in Edinburgh University archives and may used by qualified researchers for teaching and research purposes
-
-If you do not agree to all of these, please inform the experimenter now.
-
-If you agree, press the spacebar.'''
+# display welcome
 instructies(welcome)
 
-consigne = u'''In this experiment, you will be learning part of a new language.  The language is similar to English, but you will notice some differences.
+# introduce task
+instructies(intro)
 
-Your task will be to learn to translate from English into the new language.
-
-Press the spacebar to continue.
-'''
+# give detailed instructions
 instructies(consigne)
 
-consigne = u'''Instructions -- please read carefully!
-
-Now you'll see a phrase in English, and hear a speaker of the language you're learning translate it.
-
-Look at the English phrase, listen to the speaker translate it, and then click on the translation that matches what you heard the speaker say.
-
-It's important to pay close attention, so that later on you'll be able to translate on your own.
-
-Press the spacebar to continue.
-'''
-instructies(consigne)
-
-
+# do training phase
 doTraining(trainingNoms, trainingModifiers)
+# save training data
 trainingDf.to_csv(trainingFileName, index=False)
 
-
-testConsigne = u'''Instructions -- please read carefully!
-
-In the next part of the experiment, you will show what you have learned about this new language.
-
-You will see an English phrase -- it may be the SAME LENGTH phrase that you have seen before, or it may be LONGER.
-
-Look at the English phrase, and click on the translation that you think a speaker of the language WOULD BE MOST LIKELY TO SAY.
-
-Try to do as well as you can, remembering what you learned in the first part of the experiment, but don't worry if once in a while you have to guess.
-
-Press the spacebar to continue.
-'''
+# explain test phase
 instructies(testConsigne)
 
+# do test phase
 doTest(trials)
+# save test data
 testDf.to_csv(testFileName, index=False)
 
-end = u'''Thank you!
-
-Please see the experimenter, who will proceed with a questionnaire about your linguistic experience.
-
-Press the spacebar to exit the experiment.
-'''
+# display thank you
 instructies(end)
 
 core.quit()
 
-
+#########################
